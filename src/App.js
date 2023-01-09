001eeff2e942ea67240f8b435f212165b2e2e5fb
naekit <mykyta.onishchuck@code.berlin>
2023-01-09 10:38:29 -0500

    side effect for form

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react"
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login"
+import Home from "./components/Home/Home"
+import MainHeader from "./components/MainHeader/MainHeader"
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+	useEffect(() => {
+		const storedUser = localStorage.getItem("loggedIn")
+		if (storedUser === "1") {
+			setIsLoggedIn(true)
+		}
+	}, [])
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+	const loginHandler = (email, password) => {
+		// We should of course check email and password
+		// But it's just a dummy/ demo anyways
+		localStorage.setItem("loggedIn", "1")
+		setIsLoggedIn(true)
+	}
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+	const logoutHandler = () => {
+		localStorage.removeItem("loggedIn")
+		setIsLoggedIn(false)
+	}
+
+	return (
+		<React.Fragment>
+			<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+			<main>
+				{!isLoggedIn && <Login onLogin={loginHandler} />}
+				{isLoggedIn && <Home onLogout={logoutHandler} />}
+			</main>
+		</React.Fragment>
+	)
 }
 
-export default App;
+export default App
