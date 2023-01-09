8b0138ba3399c86b2e7076f604e20019f6d0b85d
naekit <mykyta.onishchuck@code.berlin>
2023-01-09 14:13:30 -0500

    context provider for authentication

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ function App() {
 	}
 
 	return (
-		<AuthContext.Provider>
-			<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+		<AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
+			<MainHeader onLogout={logoutHandler} />
 			<main>
 				{!isLoggedIn && <Login onLogin={loginHandler} />}
 				{isLoggedIn && <Home onLogout={logoutHandler} />}
